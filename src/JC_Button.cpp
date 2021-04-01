bd7e8e2ac7533cf152cf5fd1e7a05d52cf339c6d
dloyer4 <50478453+dloyer4@users.noreply.github.com>
2021-04-01 12:32:23 -0700

    Update JC_Button.cpp

--- src/JC_Button.cpp
+++ src/JC_Button.cpp
@@ -2,6 +2,7 @@
 // https://github.com/JChristensen/JC_Button
 // Copyright (C) 2018 by Jack Christensen and licensed under
 // GNU GPL v3.0, https://www.gnu.org/licenses/gpl.html
+// Modified by Dennis Loyer to work with MCP23017 I/O extender
 
 #include "JC_Button.h"
 
@@ -10,13 +11,39 @@
 /-----------------------------------------------------------------------*/
 void Button::begin()
 {
-    pinMode(m_pin, m_puEnable ? INPUT_PULLUP : INPUT);
-    m_state = digitalRead(m_pin);
-    if (m_invert) m_state = !m_state;
-    m_time = millis();
-    m_lastState = m_state;
-    m_changed = false;
-    m_lastChange = m_time;
+	
+	
+	if ((bool)m_mcp)
+	{
+		
+		if (m_puEnable)
+		{
+			m_mcp->pinMode(m_pin, INPUT);
+			m_mcp->pullUp(m_pin, HIGH);
+		}
+		else
+		{
+			m_mcp->pinMode(m_pin, INPUT);
+			m_mcp->pullUp(m_pin, LOW);
+		}
+		m_state = m_mcp->digitalRead(m_pin);
+		if (m_invert) m_state = !m_state;
+		m_time = millis();
+		m_lastState = m_state;
+		m_changed = false;
+		m_lastChange = m_time;
+	
+	}
+	else{
+		pinMode(m_pin, m_puEnable ? INPUT_PULLUP : INPUT);
+		m_state = digitalRead(m_pin);
+		if (m_invert) m_state = !m_state;
+		m_time = millis();
+		m_lastState = m_state;
+		m_changed = false;
+		m_lastChange = m_time;
+	}
+	
 }
 
 /*----------------------------------------------------------------------*
@@ -25,22 +52,49 @@ void Button::begin()
 /-----------------------------------------------------------------------*/
 bool Button::read()
 {
-    uint32_t ms = millis();
-    bool pinVal = digitalRead(m_pin);
-    if (m_invert) pinVal = !pinVal;
-    if (ms - m_lastChange < m_dbTime)
-    {
-        m_changed = false;
-    }
-    else
-    {
-        m_lastState = m_state;
-        m_state = pinVal;
-        m_changed = (m_state != m_lastState);
-        if (m_changed) m_lastChange = ms;
-    }
-    m_time = ms;
-    return m_state;
+	
+	if((bool)m_mcp)
+	{
+		
+		uint32_t ms = millis();
+		bool pinVal = m_mcp->digitalRead(m_pin);
+		if (m_invert) pinVal = !pinVal;
+		if (ms - m_lastChange < m_dbTime)
+		{
+			m_changed = false;
+		}
+		else
+		{
+			m_lastState = m_state;
+			m_state = pinVal;
+			m_changed = (m_state != m_lastState);
+			if (m_changed) m_lastChange = ms;
+		}
+		m_time = ms;
+		return m_state;
+		
+	}
+	else
+	{
+		uint32_t ms = millis();
+		bool pinVal = digitalRead(m_pin);
+		if (m_invert) pinVal = !pinVal;
+		if (ms - m_lastChange < m_dbTime)
+		{
+			m_changed = false;
+		}
+		else
+		{
+			m_lastState = m_state;
+			m_state = pinVal;
+			m_changed = (m_state != m_lastState);
+			if (m_changed) m_lastChange = ms;
+		}
+		m_time = ms;
+		return m_state;
+	}
+	
+	
 }
 
 /*----------------------------------------------------------------------*
